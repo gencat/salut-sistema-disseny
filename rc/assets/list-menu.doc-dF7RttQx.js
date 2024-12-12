0ef0654ed4a57b336e1488951341a621542d8549
ruben.gomez <ruben.gomez@accenture.com>
2024-12-12 12:25:39 +0100

    RC 2.0.0-rc.1 fix link

---
+++ rc/assets/list-menu.doc-dF7RttQx.js
@@ -0,0 +1,47 @@
+import{ae as e,af as a,ag as c,ah as i,ai as r}from"./index-Dxx82nxn.js";import{u as l}from"./index-w933pGNw.js";import{S as d,P as o,N as h,I as x,A as m,a as j,T as u,b,D as p}from"./list-menu.stories-DkO56yv_.js";import"./iframe-BDLrX2vE.js";import"../sb-preview/runtime.js";import"./index-u5I_DdWt.js";import"./index-D-8MO0q_.js";import"./index-DrFu-skq.js";import"./lit-html-B2eaWknC.js";function t(n){const s={a:"a",blockquote:"blockquote",code:"code",h2:"h2",h3:"h3",h4:"h4",li:"li",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...l(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx("style",{children:`
+   
+  `}),`
+`,e.jsx(a,{of:d}),`
+`,e.jsxs("div",{className:"dss-sb-title",children:[e.jsx(c,{}),e.jsx("dss-badge",{text:"HTML/CSS",size:"md",state:"moderate-high",outlined:!0,hideIcon:!0})]}),`
+`,e.jsx(s.p,{children:"Description lorem ipsum"}),`
+`,e.jsxs(s.blockquote,{children:[`
+`,e.jsx(s.p,{children:"Aquest és el exemple que es troba al playground. Tota modificació es veurà reflectida al següent bloc."}),`
+`]}),`
+`,e.jsx(i,{of:o}),`
+`,e.jsx(r,{}),`
+`,e.jsx(s.h2,{id:"variants",children:"Variants"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#list-menu-sense-icona",children:"List menu sense icona"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#list-menu-amb-icona",children:"List menu amb icona"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#list-menu-amb-acci%C3%B3",children:"List menu amb acció"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#list-menu-amb-titol-i-descripci%C3%B3",children:"List menu amb titol i descripció"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#list-menu-item-seleccionat",children:"List menu item seleccionat"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#list-menu-item-deshabilitat",children:"List menu item deshabilitat"})}),`
+`]}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"list-menu-sense-icona",children:"List menu sense icona"}),`
+`,e.jsx(i,{of:h}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"list-menu-amb-icona",children:"List menu amb icona"}),`
+`,e.jsx(i,{of:x}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"list-menu-amb-acció",children:"List menu amb acció"}),`
+`,e.jsx(i,{of:m}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"list-menu-amb-notificació",children:"List menu amb notificació"}),`
+`,e.jsx(i,{of:j}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"list-menu-amb-titol-i-descripció",children:"List menu amb titol i descripció"}),`
+`,e.jsx(i,{of:u}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"list-menu-item-seleccionat",children:"List menu item seleccionat"}),`
+`,e.jsxs(s.p,{children:["Afegir la classe ",e.jsx(s.strong,{children:e.jsx(s.code,{children:"dss-list-menu-item--selected"})})," a l'element de la llista."]}),`
+`,e.jsx(i,{of:b}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"list-menu-item-deshabilitat",children:"List menu item deshabilitat"}),`
+`,e.jsxs(s.p,{children:["Afegir la classe ",e.jsx(s.strong,{children:e.jsx(s.code,{children:"dss-list-menu-item--disabled"})})," a l'element de la llista."]}),`
+`,e.jsx(i,{of:p}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h4,{id:"changelog",children:"Changelog"}),`
+`,e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Versió"}),e.jsx(s.th,{children:"Canvis"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"2.0.0"})}),e.jsx(s.td,{children:"Refactor v2."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"1.12.6"})}),e.jsxs(s.td,{children:["S'han actualitzat els colors del fons per als estats 'hover', 'active' and 'selected'.",e.jsx("br",{}),"S'ha afegit l'opció d'incloure una 'insígnia de notificació' de tipus 'punt'."]})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"1.10.0"})}),e.jsxs(s.td,{children:["S'ha modificat la capçalera del component perquè s'alineï amb el disseny, transformant la icona al costat del títol en una decorative icon.",e.jsx("br",{})," S'han actualitzat els estats d'interacció amb els elements del menú per complir amb els requisits d'accessibilitat."]})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"1.7.0"})}),e.jsx(s.td,{children:"S' ha corregit l' espaiat entre els elements títol, descripció i llista."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"1.6.0"})}),e.jsx(s.td,{children:"S'ha creat el component List Menu."})]})]})]})]})}function M(n={}){const{wrapper:s}={...l(),...n.components};return s?e.jsx(s,{...n,children:e.jsx(t,{...n})}):t(n)}export{M as default};
