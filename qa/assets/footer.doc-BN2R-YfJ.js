65c379dec793be6a2964e48f8130201dcb55bddb
ruben.gomez <ruben.gomez@accenture.com>
2025-03-04 11:55:00 +0100

    Update QA to 2.2.0

---
+++ qa/assets/footer.doc-BN2R-YfJ.js
@@ -0,0 +1,18 @@
+import{j as e,M as i,T as o,C as l}from"./index-D4EretDr.js";import{useMDXComponents as n}from"./index--OTksglx.js";import{S as a,P as d}from"./footer.stories-B0G5WDHM.js";import"./iframe-jdFqwJLJ.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DxHf6ONh.js";import"./index-DrFu-skq.js";import"./lit-html-B2eaWknC.js";function r(s){const t={blockquote:"blockquote",code:"code",h3:"h3",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",...n(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx("style",{children:`
+    .innerZoomElementWrapper #root-inner {
+      width: 700px;
+    }
+  `}),`
+`,e.jsx(i,{of:a}),`
+`,e.jsxs("div",{className:"dss-sb-title",children:[e.jsx(o,{}),e.jsx("dss-badge",{text:"dss-footer",size:"md",state:"info",outlined:!0,hideIcon:!0})]}),`
+`,e.jsx(t.p,{children:`El Footer és un component senzill, net i directe, el qual mostra informació
+útil per a l'usuari que no s'ha pogut mostrar a la capçalera. Per crear un
+footer utilitzarem la següent estructura:`}),`
+`,e.jsxs(t.p,{children:["Cal tenir en compte que el ",e.jsx(t.code,{children:"dss-footer"})," s'amaga en pantalles iguals o menors a 808 píxels, per raons estrictament de diseny i usabilitat."]}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsx(t.p,{children:"Aquest és el exemple que es troba al playground. Tota modificació es veurà reflectida al següent bloc."}),`
+`]}),`
+`,e.jsx(l,{of:d}),`
+`,e.jsx(t.h3,{id:"propietats",children:"Propietats"}),`
+`,e.jsx("div",{className:"dss-sb-properties",children:e.jsxs(t.table,{children:[e.jsx(t.thead,{children:e.jsxs(t.tr,{children:[e.jsx(t.th,{children:"Name"}),e.jsx(t.th,{children:"Type"}),e.jsx(t.th,{children:"Default"}),e.jsx(t.th,{children:"Description"})]})}),e.jsxs(t.tbody,{children:[e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsxs(t.strong,{children:["logo",e.jsx("span",{children:"*"})]})}),e.jsx(t.td,{children:e.jsx(t.code,{children:"string"})}),e.jsx(t.td,{children:"-"}),e.jsx(t.td,{children:"Ruta de la imatge. Utilitzar la ruta configurada per obtenir la imatge interna de la llibreria."})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsxs(t.strong,{children:["description",e.jsx("span",{children:"*"})]})}),e.jsx(t.td,{children:e.jsx(t.code,{children:"string"})}),e.jsx(t.td,{children:"-"}),e.jsx(t.td,{children:"Descripció del footer."})]})]})]})}),`
+`,e.jsx(t.p,{children:"(*) Propietat obligatòria"})]})}function f(s={}){const{wrapper:t}={...n(),...s.components};return t?e.jsx(t,{...s,children:e.jsx(r,{...s})}):r(s)}export{f as default};
