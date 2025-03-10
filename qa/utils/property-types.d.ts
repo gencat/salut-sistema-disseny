42a5ea28845fe1ed7d0a944d3aa59e052ee567ff
ruben.gomez <ruben.gomez@accenture.com>
2025-03-10 12:33:15 +0100

    Update QA v2.2.0

--- qa/utils/property-types.d.ts
+++ qa/utils/property-types.d.ts
@@ -3,3 +3,4 @@ import { PropertyDeclaration } from 'lit';
 export declare const booleanType: PropertyDeclaration<boolean>;
 export declare const selectedType: PropertyDeclaration<string[]>;
 export declare const arrayType: PropertyDeclaration<string[]>;
+export type FontWeight = 'inherit' | 'regular' | 'semibold' | 'bold';
