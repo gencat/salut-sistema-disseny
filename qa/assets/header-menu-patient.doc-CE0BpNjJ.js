472eff8b1c9c34947cea75fb886123fce77e118b
ruben.gomez <ruben.gomez@accenture.com>
2025-03-20 11:48:50 +0100

    QA v2.2.1

---
+++ qa/assets/header-menu-patient.doc-CE0BpNjJ.js
@@ -0,0 +1,26 @@
+import{j as e,M as d,T as i,C as l,a as c}from"./index-BauLvQ7S.js";import{useMDXComponents as r}from"./index-DL_N9JPE.js";import{S as h,P as o}from"./header-menu-patient.stories-T8a-3A_A.js";import"./iframe-Bg6vOPaM.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DxHf6ONh.js";import"./index-DrFu-skq.js";import"./lit-html-B2eaWknC.js";function t(s){const n={blockquote:"blockquote",code:"code",h3:"h3",h4:"h4",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",...r(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx("style",{children:`
+    .innerZoomElementWrapper {
+      display: flex;
+      justify-content: center;
+      align-items: flex-start;
+      min-height: 300px;
+    }
+  `}),`
+`,e.jsx(d,{of:h}),`
+`,e.jsxs("div",{className:"dss-sb-title",children:[e.jsx(i,{}),e.jsx("dss-badge",{text:"dss-header-menu-patient",size:"md",state:"info",outlined:!0,hideIcon:!0})]}),`
+`,e.jsx(n.p,{children:"Description lorem ipsum"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:"Aquest és el exemple que es troba al playground. Tota modificació es veurà reflectida al següent bloc."}),`
+`]}),`
+`,e.jsx(l,{of:o}),`
+`,e.jsx(c,{}),`
+`,e.jsx(n.h3,{id:"propietats",children:"Propietats"}),`
+`,e.jsx("div",{className:"dss-sb-properties",children:e.jsxs(n.table,{children:[e.jsx(n.thead,{children:e.jsxs(n.tr,{children:[e.jsx(n.th,{children:"Name"}),e.jsx(n.th,{children:"Type"}),e.jsx(n.th,{children:"Default"}),e.jsx(n.th,{children:"Description"})]})}),e.jsxs(n.tbody,{children:[e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsx(n.strong,{children:"size"})}),e.jsx(n.td,{children:e.jsx(n.code,{children:"string"})}),e.jsx(n.td,{children:"'lg'"}),e.jsx(n.td,{children:"Determineu el max-width del contenidor principal del component."})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsx(n.strong,{children:"disabled"})}),e.jsx(n.td,{children:e.jsx(n.code,{children:"boolean"})}),e.jsx(n.td,{children:"false"}),e.jsx(n.td,{children:"Deshabiliteu el botó per obrir o tancar el dropdown."})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsxs(n.strong,{children:["name",e.jsx("span",{children:"*"})]})}),e.jsx(n.td,{children:e.jsx(n.code,{children:"string"})}),e.jsx(n.td,{children:"-"}),e.jsx(n.td,{children:"Nom del pacient"})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsxs(n.strong,{children:["cip",e.jsx("span",{children:"*"})]})}),e.jsx(n.td,{children:e.jsx(n.code,{children:"string"})}),e.jsx(n.td,{children:"-"}),e.jsx(n.td,{children:"CIP del pacient"})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsxs(n.strong,{children:["age",e.jsx("span",{children:"*"})]})}),e.jsx(n.td,{children:e.jsx(n.code,{children:"string"})}),e.jsx(n.td,{children:"-"}),e.jsx(n.td,{children:"Edat del pacient"})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsxs(n.strong,{children:["gender",e.jsx("span",{children:"*"})]})}),e.jsx(n.td,{children:e.jsx(n.code,{children:"string"})}),e.jsx(n.td,{children:"-"}),e.jsx(n.td,{children:"Gènere del pacient"})]})]})]})}),`
+`,e.jsx(n.p,{children:"(*) Propietat obligatòria"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h3,{id:"esdeveniments",children:"Esdeveniments"}),`
+`,e.jsxs(n.table,{children:[e.jsx(n.thead,{children:e.jsxs(n.tr,{children:[e.jsx(n.th,{children:"Name"}),e.jsx(n.th,{children:"Description"})]})}),e.jsxs(n.tbody,{children:[e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsx(n.strong,{children:"onActionPatientClick"})}),e.jsx(n.td,{children:"Esdeveniment que notifica quan s'ha fet clic en alguna de les accions del menú desplegable."})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsx(n.strong,{children:"onCopyCIP"})}),e.jsx(n.td,{children:"Esdeveniment que retorna el codi CIP del pacient. Utilitzar només en cas que la funcionalitat interna del component per capturar el CIP al porta-retalls no s'executi correctament."})]})]})]}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h4,{id:"changelog",children:"Changelog"}),`
+`,e.jsxs(n.table,{children:[e.jsx(n.thead,{children:e.jsxs(n.tr,{children:[e.jsx(n.th,{children:"Versió"}),e.jsx(n.th,{children:"Canvis"})]})}),e.jsx(n.tbody,{children:e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsx(n.strong,{children:"1.12.0"})}),e.jsx(n.td,{children:"Nou web component."})]})})]})]})}function C(s={}){const{wrapper:n}={...r(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(t,{...s})}):t(s)}export{C as default};
