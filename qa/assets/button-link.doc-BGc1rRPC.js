ca99ff690f3fa325d426b212740d2dbf5232929b
ruben.gomez <ruben.gomez@accenture.com>
2025-04-02 09:56:51 +0200

    Update svg import for v2.3.0

---
+++ qa/assets/button-link.doc-BGc1rRPC.js
@@ -0,0 +1,45 @@
+import{j as e,M as d,T as l,C as i,a}from"./index-D7WtWWmQ.js";import{useMDXComponents as r}from"./index-BaXe3uBm.js";import{S as c,P as o,B as h,I as x,D as j}from"./button-link.stories-CTbjH0_o.js";import"./iframe-DPrRSrhm.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DxHf6ONh.js";import"./index-DrFu-skq.js";import"./lit-element-DhY7bShc.js";import"./lit-html-B2eaWknC.js";import"./button-link-DkjJh6Te.js";import"./property-DnLABHHV.js";import"./icon.style-DPTab5RN.js";import"./reset.style-B0CQ8619.js";import"./property-types-CkH-zh1n.js";import"./class-map-BDeD7Kfm.js";import"./static-B9bHPl91.js";import"./custom-element-scope-Dp4Dg8bo.js";function t(s){const n={a:"a",blockquote:"blockquote",code:"code",h2:"h2",h3:"h3",h4:"h4",li:"li",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...r(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx("style",{children:`
+   .none {
+      display:none;
+    }
+    .wrapper {
+      display: flex;
+      align-items: center;
+      gap: 10px;
+    }
+  `}),`
+`,e.jsx(d,{of:c}),`
+`,e.jsxs("div",{className:"dss-sb-title",children:[e.jsx(l,{}),e.jsx("dss-badge",{text:"dss-button-link",size:"md",state:"moderate-high",outlined:!0,hideIcon:!0})]}),`
+`,e.jsx(n.p,{children:"Els Buttons Link són elements clicables que s'utilitzen per navegar a través de les diferents seccions de l'aplicació."}),`
+`,e.jsx(n.p,{children:"Per simplificar el seu ús i fer-lo més intuïtiu, aquest component s'ha dividit en:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"dss-link"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"dss-button:variant=link"})}),`
+`]}),`
+`,e.jsx(n.p,{children:"D'aquesta manera, s'evita la confusió d'utilitzar un enllaç de forma incorrecta o viceversa, ja que un enllaç s'ha d'utilitzar per gestionar la navegació per l'aplicació, mentre que un botó ha de capturar el clic per executar algun tipus de funcionalitat addicional."}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:"Aquest és el exemple que es troba al playground. Tota modificació es veurà reflectida al següent bloc."}),`
+`]}),`
+`,e.jsx(i,{of:o}),`
+`,e.jsx(a,{}),`
+`,e.jsx(n.h3,{id:"propietats",children:"Propietats"}),`
+`,e.jsxs(n.table,{children:[e.jsx(n.thead,{children:e.jsxs(n.tr,{children:[e.jsx(n.th,{children:"Name"}),e.jsx(n.th,{children:"Type"}),e.jsx(n.th,{children:"Default"}),e.jsx(n.th,{children:"Description"})]})}),e.jsxs(n.tbody,{children:[e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsx(n.strong,{children:"label"})}),e.jsx(n.td,{children:e.jsx(n.code,{children:"string"})}),e.jsx(n.td,{children:"''"}),e.jsx(n.td,{children:"Text que es mostra dins del component."})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsx(n.strong,{children:"url"})}),e.jsx(n.td,{children:e.jsx(n.code,{children:"string"})}),e.jsx(n.td,{children:"''"}),e.jsx(n.td,{children:"Enllaç que usés el component com a atribut Href."})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsx(n.strong,{children:"icon"})}),e.jsx(n.td,{children:e.jsx(n.code,{children:"string"})}),e.jsx(n.td,{children:"''"}),e.jsx(n.td,{children:"Icona mostrada dins del component."})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsx(n.strong,{children:"iconPosition"})}),e.jsx(n.td,{children:e.jsx(n.code,{children:"string"})}),e.jsx(n.td,{children:"'left'"}),e.jsx(n.td,{children:"Posició de la icona en relació amb el text. Pot ser: left o right."})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsx(n.strong,{children:"disabled"})}),e.jsx(n.td,{children:e.jsx(n.code,{children:"boolean"})}),e.jsx(n.td,{children:"false"}),e.jsxs(n.td,{children:["Dóna al component un estil de deshabilitat. Aquest component és un enllaç ",e.jsx(n.code,{children:"<a>"}),", per tant, a nivell de desenvolupament no es pot deshabilitar.",e.jsx("br",{}),"L'opció ideal seria inhabilitar l'href (linkHref) si aquesta propietat està habilitada añadiendo ",e.jsx(n.code,{children:"javascript:void(0)"})]})]})]})]}),`
+`,e.jsx(n.p,{children:"(*) Propietat obligatòria"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h2,{id:"variants",children:"Variants"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#per-defecte",children:"Per defecte"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#amb-icona",children:"Amb icona"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"#deshabilitat",children:"Deshabilitat"})}),`
+`]}),`
+`,e.jsx(n.h3,{id:"per-defecte",children:"Per defecte"}),`
+`,e.jsx(i,{of:h}),`
+`,e.jsx(n.h3,{id:"amb-icona",children:"Amb icona"}),`
+`,e.jsx(i,{of:x}),`
+`,e.jsx(n.h3,{id:"deshabilitat",children:"Deshabilitat"}),`
+`,e.jsxs(n.p,{children:["L'etiqueta ",e.jsx(n.strong,{children:e.jsx(n.code,{children:"a"})})," no disposa de la propietat disabled per la qual cosa haurem de fer ús de la classe ",e.jsx(n.strong,{children:e.jsx(n.code,{children:"dss-button-link--disabled"})}),"."]}),`
+`,e.jsx(i,{of:j}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h4,{id:"changelog",children:"Changelog"}),`
+`,e.jsxs(n.table,{children:[e.jsx(n.thead,{children:e.jsxs(n.tr,{children:[e.jsx(n.th,{children:"Versió"}),e.jsx(n.th,{children:"Canvis"})]})}),e.jsxs(n.tbody,{children:[e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsx(n.strong,{children:"1.11.0"})}),e.jsx(n.td,{children:"Nou web component."})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{children:e.jsx(n.strong,{children:"1.7.0"})}),e.jsx(n.td,{children:"Nou component simple per donar un estil específic als enllaços."})]})]})]})]})}function L(s={}){const{wrapper:n}={...r(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(t,{...s})}):t(s)}export{L as default};
