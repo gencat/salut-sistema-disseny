d9631af545b11834d427694d18dc7c49bac90ef6
ruben.gomez <ruben.gomez@accenture.com>
2025-06-27 10:32:57 +0200

    v2.7.0

---
+++ assets/header-menu-professional.doc-D3-ustw_.js
@@ -0,0 +1,44 @@
+import{j as e,M as l,T as t,C as d,a as c}from"./index-C93cOCZL.js";import{useMDXComponents as i}from"./index-CIGanLMC.js";import{S as o,P as a,R as h,N as x}from"./header-menu-professional.stories-B4LxhVVF.js";import"./iframe-CxJdfFi9.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-C_pJYAGD.js";import"./index-DxHf6ONh.js";import"./index-DrFu-skq.js";import"./lit-html-D6a8R3xZ.js";function r(n){const s={a:"a",blockquote:"blockquote",code:"code",em:"em",h3:"h3",h4:"h4",li:"li",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...i(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx("style",{children:`
+    .innerZoomElementWrapper {
+      display: flex;
+      justify-content: center;
+      align-items: flex-start;
+      min-height: 600px;
+    }
+  `}),`
+`,e.jsx(l,{of:o}),`
+`,e.jsxs("div",{className:"dss-sb-title",children:[e.jsx(t,{}),e.jsx("dss-badge",{text:"dss-header-menu-professional",size:"md",state:"info",outlined:!0,hideIcon:!0})]}),`
+`,e.jsx(s.p,{children:"Component intern del Header"}),`
+`,e.jsxs(s.p,{children:[e.jsx(s.strong,{children:"Slots:"}),' Aquest component inclou un slot="avatar" per poder injectar el component dss-avatar en el mateix.']}),`
+`,e.jsx(s.p,{children:"Al mateix temps, disposa d'un slot per defecte, que haurem d'utilitzar per incloure a la zona designada els diferents input-dropdowns que s'utilitzaran per modificar les preferències de treball."}),`
+`,e.jsxs(s.blockquote,{children:[`
+`,e.jsx(s.p,{children:"Aquest és el exemple que es troba al playground. Tota modificació es veurà reflectida al següent bloc."}),`
+`]}),`
+`,e.jsx(d,{of:a}),`
+`,e.jsx(c,{}),`
+`,e.jsx(s.h3,{id:"propietats",children:"Propietats"}),`
+`,e.jsx("div",{className:"dss-sb-properties",children:e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Name"}),e.jsx(s.th,{children:"Type"}),e.jsx(s.th,{children:"Default"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"disabled"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"boolean"})}),e.jsx(s.td,{children:"false"}),e.jsx(s.td,{children:"Deshabiliteu el botó per obrir o tancar el dropdown."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsxs(s.strong,{children:["name",e.jsx("span",{children:"*"})]})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"string"})}),e.jsx(s.td,{children:"-"}),e.jsx(s.td,{children:"Nom del professional."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsxs(s.strong,{children:["center",e.jsx("span",{children:"*"})]})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"string"})}),e.jsx(s.td,{children:"-"}),e.jsx(s.td,{children:"Nom del centre hospitalari."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsxs(s.strong,{children:["collegiate",e.jsx("span",{children:"*"})]})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"string"})}),e.jsx(s.td,{children:"-"}),e.jsx(s.td,{children:"Nom del col·legiat."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"infoLabel"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"string"})}),e.jsx(s.td,{children:"'Les meves dades'"}),e.jsx(s.td,{children:"Label per secció d'informació."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"collegiateLabel"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"string"})}),e.jsx(s.td,{children:"-"}),e.jsx(s.td,{children:"Label per nom del col·legiat."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"preferencesLabel"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"string"})}),e.jsx(s.td,{children:"'Col·legiat nº'"}),e.jsx(s.td,{children:"Label per secció de preferències."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"detailsLabel"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"string"})}),e.jsx(s.td,{children:"'Veure més detalls'"}),e.jsx(s.td,{children:"Text enllaç details."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"exitLabel"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"string"})}),e.jsx(s.td,{children:"'Sortir'"}),e.jsx(s.td,{children:"Text botó sortir."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"showLogout"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"boolean"})}),e.jsx(s.td,{children:"false"}),e.jsx(s.td,{children:"Mostra botó tancar secció."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"logoutLabel"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"string"})}),e.jsx(s.td,{children:"'Tancar sessió'"}),e.jsx(s.td,{children:"Text botó tancar secció."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"hideDropdownDetails"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"boolean"})}),e.jsx(s.td,{children:"false"}),e.jsx(s.td,{children:"Amaga la secció d'informació personal del professional."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"hideDropdownPreferences"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"boolean"})}),e.jsx(s.td,{children:"false"}),e.jsx(s.td,{children:"Amaga la secció de preferències de treball."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"hideViewDetails"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"boolean"})}),e.jsx(s.td,{children:"false"}),e.jsx(s.td,{children:'Amaga el botó de "Veure més detalls".'})]})]})]})}),`
+`,e.jsx(s.p,{children:"(*) Propietat obligatòria"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"esdeveniments",children:"Esdeveniments"}),`
+`,e.jsx("div",{className:"dss-sb-properties",children:e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Name"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"onViewDetails"})}),e.jsx(s.td,{children:"Notifica quan s'ha fet clic al botó de veure més detalls del pacient."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"onExit"})}),e.jsx(s.td,{children:"Notifica quan s'ha fet clic al botó de sortir"})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"onLogout"})}),e.jsx(s.td,{children:"Notifica quan s'ha fet clic al botó de tancar sessió"})]})]})]})}),`
+`,e.jsx(s.h3,{id:"variants",children:"Variants"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#versi%C3%B3-redu%C3%AFda",children:"Versió Reduïda"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#enlla%C3%A7-a-detalls-no-disponible",children:"Enllaç a detalls no disponible"})}),`
+`]}),`
+`,e.jsx(s.h4,{id:"versió-reduïda",children:"Versió Reduïda"}),`
+`,e.jsx(s.p,{children:"Aquesta variant s'utilitzarà en aquelles iniciatives que no disposen d'accés al servidor de recursos per obtenir tota la informació disponible."}),`
+`,e.jsx(s.p,{children:"Els passos a seguir són:"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsxs(s.li,{children:["Incloure els atributs ",e.jsx(s.code,{children:"hideDropdownDetails"})," i ",e.jsx(s.code,{children:"hideDropdownPreferences"}),"."]}),`
+`,e.jsxs(s.li,{children:["Eliminar l'atribut ",e.jsx(s.code,{children:"collegiate"}),"."]}),`
+`,e.jsxs(s.li,{children:["Eliminar els ",e.jsx(s.em,{children:"input-dropdowns"})," de l'interior del component."]}),`
+`]}),`
+`,e.jsx(d,{of:h}),`
+`,e.jsx(s.h4,{id:"enllaç-a-detalls-no-disponible",children:"Enllaç a detalls no disponible"}),`
+`,e.jsxs(s.p,{children:["Per ocultar l'enllaç a la pàgina de detalls del professional, cal assegurar-se de no incloure l'atribut ",e.jsx(s.code,{children:"detailsHref"}),"."]}),`
+`,e.jsx(d,{of:x}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h4,{id:"changelog",children:"Changelog"}),`
+`,e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Versió"}),e.jsx(s.th,{children:"Canvis"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"2.3.0"})}),e.jsx(s.td,{children:"S'han modificat les accions del component, tenint ara per defecte un botó per sortir amb el seu esdeveniment onExit, i l'antic botó per tancar sessió amb el seu esdeveniment onLogout ara està ocult per defecte. S'ha modificat l'enllaç per veure més detalls del professional, que ara és un botó amb el seu esdeveniment onViewDetails."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"1.12.0"})}),e.jsx(s.td,{children:"Nou web component."})]})]})]})]})}function q(n={}){const{wrapper:s}={...i(),...n.components};return s?e.jsx(s,{...n,children:e.jsx(r,{...n})}):r(n)}export{q as default};
