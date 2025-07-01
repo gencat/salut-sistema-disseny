c864ac65417b0eccca11a6ffe8a41a5ebcc88b8c
ruben.gomez <ruben.gomez@accenture.com>
2025-07-01 14:16:54 +0200

    Fix animations inner style

---
+++ assets/popover.doc-BdzsNgUT.js
@@ -0,0 +1,64 @@
+import{j as e,M as d,T as l,C as n,a as o}from"./index-CuBtzvfW.js";import{useMDXComponents as t}from"./index-BDSzqcmZ.js";import{S as a,P as c,B as h,I as x,a as j,b as p,c as m,d as u}from"./popover.stories-B57QPgLu.js";import"./iframe-DnZgGT9z.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-C_pJYAGD.js";import"./index-DxHf6ONh.js";import"./index-DrFu-skq.js";import"./lit-html-D6a8R3xZ.js";function r(i){const s={a:"a",blockquote:"blockquote",code:"code",em:"em",h2:"h2",h3:"h3",h4:"h4",h5:"h5",li:"li",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...t(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx("style",{children:`
+   .innerZoomElementWrapper{
+      height: 500px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .demo-wrapper {
+      position: relative;
+      width: fit-content;
+    }
+    .demo-related-content {
+      font-size: 12px;
+      color: lightgray;
+    }
+  `}),`
+`,e.jsx(d,{of:a}),`
+`,e.jsxs("div",{className:"dss-sb-title",children:[e.jsx(l,{}),e.jsx("dss-badge",{text:"dss-popover",size:"md",state:"info",outlined:!0,hideIcon:!0})]}),`
+`,e.jsx(s.p,{children:"Un Popover mostra als usuaris que hi ha més informació per completar el flux de treball."}),`
+`,e.jsxs(s.p,{children:["Dentro del component podem utilitzar els slots 'body' o 'item-list' per definir dos tipus de contingut. Per a això podem fer ús del subcomponent ",e.jsx(s.code,{children:'<dss-popover-body slot="body">'})," o del component ",e.jsx(s.code,{children:'<dss-item-list slot="item-list">'}),", cadascun amb les seves respectives propietats."]}),`
+`,e.jsxs(s.blockquote,{children:[`
+`,e.jsx(s.p,{children:"Aquest és el exemple que es troba al playground. Tota modificació es veurà reflectida al següent bloc."}),`
+`]}),`
+`,e.jsx(n,{of:c}),`
+`,e.jsx(o,{}),`
+`,e.jsx(s.h3,{id:"propietats",children:"Propietats"}),`
+`,e.jsx("div",{className:"dss-sb-properties",children:e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Name"}),e.jsx(s.th,{children:"Type"}),e.jsx(s.th,{children:"Default"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsxs(s.strong,{children:["title",e.jsx("span",{children:"*"})]})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"string"})}),e.jsx(s.td,{children:"-"}),e.jsx(s.td,{children:"Titol que es mostra dins del component."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsxs(s.strong,{children:["actionIcon",e.jsx("span",{children:"*"})]})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"string"})}),e.jsx(s.td,{children:"-"}),e.jsx(s.td,{children:"Mostra una icona que permet executar una acció."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"hasDivider"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"boolean"})}),e.jsx(s.td,{children:"false"}),e.jsx(s.td,{children:"Mostra un divisor entre la capçalera i el contingut del popover."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"hideCloseIcon"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"boolean"})}),e.jsx(s.td,{children:"false"}),e.jsx(s.td,{children:"Amaga la icona per tancar el component."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"position"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"string"})}),e.jsx(s.td,{children:"'top'"}),e.jsx(s.td,{children:"Defineix la posició del popover dins de l’element relacionat. Pot ser: top, left, bottom, right."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"open"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"boolean"})}),e.jsx(s.td,{children:"false"}),e.jsx(s.td,{children:"Mostra el component quan aquesta propietat està definida."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"disableParentClick"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"boolean"})}),e.jsx(s.td,{children:"false"}),e.jsx(s.td,{children:"Desactiva la funcionalitat interna per mostrar el component quan es fa clic al contenidor pare."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"popoverFixed"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"boolean"})}),e.jsx(s.td,{children:"false"}),e.jsx(s.td,{children:"Modifica l'estratègia de posicionament del desplegable a fixed en lloc d'absolute. Utilitza-ho només en casos on el desplegable no apareix correctament perquè està afectat per algun position: absolute o overflow:hidden."})]})]})]})}),`
+`,e.jsx(s.p,{children:"(*) Propietat obligatòria"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"esdeveniments",children:"Esdeveniments"}),`
+`,e.jsx("div",{className:"dss-sb-properties",children:e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Name"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"onAction"})}),e.jsx(s.td,{children:"Esdeveniment que es llança quan es fa clic a la icona d'acció."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"onClose"})}),e.jsx(s.td,{children:"Esdeveniment que es llança quan es fa clic a la icona per tancar el popover."})]})]})]})}),`
+`,e.jsx(s.h3,{id:"variants",children:"Variants"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#default",children:"Default"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#item-list",children:"Item List"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#posici%C3%B3",children:"Posició"})}),`
+`]}),`
+`,e.jsx("hr",{}),`
+`,e.jsx(s.h3,{id:"default",children:"Default"}),`
+`,e.jsx(n,{of:h}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"item-list",children:"Item List"}),`
+`,e.jsx(n,{of:x}),`
+`,e.jsx(s.h3,{id:"posició",children:"Posició"}),`
+`,e.jsx(s.p,{children:e.jsxs(s.em,{children:[e.jsx(s.strong,{children:"Nota:"})," En aquest exemple farem servir la propietat 'position'."]})}),`
+`,e.jsx(s.h5,{id:"superior",children:"Superior"}),`
+`,e.jsx(n,{of:j}),`
+`,e.jsx(s.h5,{id:"dreta",children:"Dreta"}),`
+`,e.jsx(n,{of:p}),`
+`,e.jsx(s.h5,{id:"inferior",children:"Inferior"}),`
+`,e.jsx(n,{of:m}),`
+`,e.jsx(s.h5,{id:"esquerra",children:"Esquerra"}),`
+`,e.jsx(n,{of:u}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"ús",children:"Ús"}),`
+`,e.jsx(s.h5,{id:"mida-màxima-amplada-i-alçada",children:"Mida màxima: amplada i alçada"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:"Min-width: 300px"}),`
+`,e.jsx(s.li,{children:"Max-width: 500px"}),`
+`,e.jsx(s.li,{children:"Max-height: 480px (Si el contingut del popover supera l'altura màxima permesa, apareixerà una barra de desplaçament a l'interior del component.)"}),`
+`]}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h4,{id:"changelog",children:"Changelog"}),`
+`,e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Versió"}),e.jsx(s.th,{children:"Canvis"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"2.2.0"})}),e.jsx(s.td,{children:"S'ha corregit el posicionament i l'ombra de la fletxa indicadora que apareix en desplegar el popover."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"2.0.3"})}),e.jsx(s.td,{children:"S'ha afegit l'esdeveniment onClose quan el popover es tanca de qualsevol manera, no només fent clic a icona de tancar."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"2.0.2"})}),e.jsx(s.td,{children:"S'ha corregit un error de posicionament després que l'element pare s'hagi desplaçat a la pantalla."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"2.0.0"})}),e.jsx(s.td,{children:"Refactor v2."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"1.11.0"})}),e.jsx(s.td,{children:"Nou web component."})]})]})]})]})}function C(i={}){const{wrapper:s}={...t(),...i.components};return s?e.jsx(s,{...i,children:e.jsx(r,{...i})}):r(i)}export{C as default};
---
+++ assets/versions.doc-sSv3Mvuc.js
@@ -0,0 +1,52 @@
+import{j as e,M as c}from"./index-CuBtzvfW.js";import{useMDXComponents as i}from"./index-BDSzqcmZ.js";import"./DocsRenderer-CFRXHY34-H9-HKe1c.js";import"./iframe-DnZgGT9z.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-C_pJYAGD.js";import"./index-DxHf6ONh.js";import"./index-DrFu-skq.js";function n(l){const s={a:"a",code:"code",h2:"h2",h3:"h3",li:"li",ol:"ol",p:"p",pre:"pre",ul:"ul",...i(),...l.components};return e.jsxs(e.Fragment,{children:[e.jsx(c,{title:"Versions"}),`
+`,e.jsx(s.h2,{id:"versió-actual",children:"Versió actual"}),`
+`,e.jsx("dss-badge",{text:"2.7.0",size:"lg",state:"ideal",hideIcon:!0}),`
+`,e.jsx(s.h2,{id:"versions-legacy",children:"Versions 'legacy'"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/2.6.0",rel:"nofollow",children:"v2.6.0"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/2.5.1",rel:"nofollow",children:"v2.5.1"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/2.5.0",rel:"nofollow",children:"v2.5.0"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/2.4.1",rel:"nofollow",children:"v2.4.1"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/2.4.0",rel:"nofollow",children:"v2.4.0"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/2.3.0",rel:"nofollow",children:"v2.3.0"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/2.2.1",rel:"nofollow",children:"v2.2.1"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/2.2.0",rel:"nofollow",children:"v2.2.0"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/2.1.1",rel:"nofollow",children:"v2.1.1"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/2.1.0",rel:"nofollow",children:"v2.1.0"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/2.0.5",rel:"nofollow",children:"v2.0.5"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/2.0.4",rel:"nofollow",children:"v2.0.4"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/2.0.3",rel:"nofollow",children:"v2.0.3"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/2.0.2",rel:"nofollow",children:"v2.0.2"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/2.0.1",rel:"nofollow",children:"v2.0.1"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/2.0.0",rel:"nofollow",children:"v2.0.0"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/legacy/1.12.8",rel:"nofollow",children:"v1.12.8"})}),`
+`]}),`
+`,e.jsx(s.h2,{id:"versions-release-candidate",children:"Versions 'release candidate'"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/builds/salut-design-system-salut-2.3.0-rc.tgz",rel:"nofollow",children:"v2.3.0-rc"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/builds/salut-design-system-salut-2.2.1-rc.tgz",rel:"nofollow",children:"v2.2.1-rc"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/builds/salut-design-system-salut-2.0.5-rc.tgz",rel:"nofollow",children:"v2.0.5-rc"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://codisisdisseny.salut.gencat.cat/builds/salut-design-system-salut-2.0.4-rc.tgz",rel:"nofollow",children:"v2.0.4-rc"})}),`
+`]}),`
+`,e.jsx(s.h3,{id:"installar--release-candidate-en-local",children:"Instal·lar  Release Candidate en local"}),`
+`,e.jsxs(s.ol,{children:[`
+`,e.jsxs(s.li,{children:[`
+`,e.jsx(s.p,{children:"Descarregar el paquet de la versió RC desitjada."}),`
+`]}),`
+`,e.jsxs(s.li,{children:[`
+`,e.jsxs(s.p,{children:["Modificar el fitxer ",e.jsx(s.code,{children:"package.json"})," del projecte on es vulgui instal·lar la versió RC, afegint la ruta del fitxer descarregat com a dependència."]}),`
+`]}),`
+`]}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{children:`"@salut/design-system-salut": "file:/Users/ruben.gomez/Downloads/salut-design-system-salut-2.2.1-rc.tgz",
+`})}),`
+`,e.jsxs(s.ol,{start:"3",children:[`
+`,e.jsxs(s.li,{children:[`
+`,e.jsxs(s.p,{children:["Elininar la carpeta ",e.jsx(s.code,{children:"node_modules/@salut"})," i el fitxer ",e.jsx(s.code,{children:"package-lock.json"})," del projecte."]}),`
+`]}),`
+`,e.jsxs(s.li,{children:[`
+`,e.jsxs(s.p,{children:["Opcional. Borrar la cache de npm per evitar problemes de dependències. ",e.jsx(s.code,{children:"npm cache clear -f"})]}),`
+`]}),`
+`,e.jsxs(s.li,{children:[`
+`,e.jsxs(s.p,{children:["Instalar les dependències del projecte. ",e.jsx(s.code,{children:"npm install"})]}),`
+`]}),`
+`]})]})}function g(l={}){const{wrapper:s}={...i(),...l.components};return s?e.jsx(s,{...l,children:e.jsx(n,{...l})}):n(l)}export{g as default};
