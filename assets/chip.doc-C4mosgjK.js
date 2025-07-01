496bc8a74985df3ec6615da237e2aea8756db8ed
ruben.gomez <ruben.gomez@accenture.com>
2025-07-01 14:06:41 +0200

    Fix animations inner style

---
+++ assets/chip.doc-C4mosgjK.js
@@ -0,0 +1,57 @@
+import{j as e,M as d,T as c,C as i,a as l}from"./index-DcCwtgtA.js";import{useMDXComponents as t}from"./index-CpKcFQy_.js";import{S as a,P as h,a as o,B as x,I as j,D as p,b as m,O as b}from"./chip.stories-C2Z7ve1_.js";import"./iframe-DZFPKzkt.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-C_pJYAGD.js";import"./index-DxHf6ONh.js";import"./index-DrFu-skq.js";import"./lit-html-D6a8R3xZ.js";function r(n){const s={a:"a",blockquote:"blockquote",code:"code",h3:"h3",h4:"h4",li:"li",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...t(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx("style",{children:`
+    .wrapper {
+      display: flex;
+      justify-content: center;
+      align-items: flex-start;
+      gap: 20px;
+    }
+    .wrapper--next {
+      margin-top: 24px;
+    }
+  `}),`
+`,e.jsx(d,{of:a}),`
+`,e.jsxs("div",{className:"dss-sb-title",children:[e.jsx(c,{}),e.jsx("dss-badge",{text:"dss-chip",size:"md",state:"info",outlined:!0,hideIcon:!0})]}),`
+`,e.jsx("hr",{}),`
+`,e.jsx(s.p,{children:`Els Chips ajuden les persones a introduir informació, fer seleccions, filtrar
+contingut o activar accions.`}),`
+`,e.jsxs(s.blockquote,{children:[`
+`,e.jsx(s.p,{children:"Aquest és el exemple que es troba al canvas. Tota modificació es veurà reflectida al següent bloc."}),`
+`]}),`
+`,e.jsx(i,{of:h}),`
+`,e.jsx(l,{}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"propietats",children:"Propietats"}),`
+`,e.jsx("div",{className:"dss-sb-properties",children:e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Name"}),e.jsx(s.th,{children:"Type"}),e.jsx(s.th,{children:"Default"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsxs(s.strong,{children:["label",e.jsx("span",{children:"*"})]})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"string"})}),e.jsx(s.td,{children:"-"}),e.jsx(s.td,{children:"Text que es mostra dins del chip."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"size"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"string"})}),e.jsx(s.td,{children:"'lg'"}),e.jsx(s.td,{children:"Mida del chip. Pot ser: xs, sm o lg."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"icon"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"string"})}),e.jsx(s.td,{children:"''"}),e.jsx(s.td,{children:"Icona mostrada dins del chio."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"selected"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"boolean"})}),e.jsx(s.td,{children:"false"}),e.jsx(s.td,{children:"Assigna al chip l'estat seleccionat."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"disabled"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"boolean"})}),e.jsx(s.td,{children:"false"}),e.jsx(s.td,{children:"Deshabilita el chip."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"disableSelect"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"boolean"})}),e.jsx(s.td,{children:"false"}),e.jsx(s.td,{children:"Deshabilita la funcionalitat de marcar o desmarcar el chip com a seleccionat."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"hasDelete"})}),e.jsx(s.td,{children:e.jsx(s.code,{children:"boolean"})}),e.jsx(s.td,{children:"false"}),e.jsx(s.td,{children:"Mostra un botó que, en fer clic, retorna un esdeveniment informant que aquest chip ha de ser eliminat des de la iniciativa."})]})]})]})}),`
+`,e.jsx(s.p,{children:"(*) Propietat obligatòria"}),`
+`,e.jsx(s.h3,{id:"esdeveniments",children:"Esdeveniments"}),`
+`,e.jsx("div",{className:"dss-sb-properties",children:e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Name"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"onToggle"})}),e.jsx(s.td,{children:"Notifica quan s'ha modificat el valor de la propietat selected, retornant la label de l'element i l'estat de selected."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"onDelete"})}),e.jsx(s.td,{children:"Notifica quan s'ha fet clic al xip i retorna si l'estat és seleccionat o no."})]})]})]})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"variants",children:"Variants"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#mida-i-tipus",children:"Mida i tipus"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#chip-b%C3%A0sic",children:"Chip bàsic"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#chip-amb-icona",children:"Chip amb icona"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#chip-amb-bot%C3%B3-de-tancar",children:"Chip amb botó de tancar"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#chip-amb-icona-i-bot%C3%B3-de-tancar",children:"Chip amb icona i icon button"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"#chip-nom%C3%A9s-icona",children:"Chip només icona"})}),`
+`]}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h3,{id:"mida-i-tipus",children:"Mida i tipus"}),`
+`,e.jsxs(s.p,{children:[`Hi ha dos tipus de mida, el mitjà i el petit, per defecte es mostrés el mitjà.
+Per mostrar el petit haurem d'afegir la propietat `,e.jsx(s.code,{children:'size="sm"'}),`. Així com el
+xip per defecte o actiu, per mostrar el xip actiu haurem d'afegir la propietat `,e.jsx(s.code,{children:"selected"})]}),`
+`,e.jsx(i,{of:o}),`
+`,e.jsx(s.h3,{id:"chip-bàsic",children:"Chip bàsic"}),`
+`,e.jsx(i,{of:x}),`
+`,e.jsx(s.h3,{id:"chip-amb-icona",children:"Chip amb icona"}),`
+`,e.jsx(i,{of:j}),`
+`,e.jsx(s.h3,{id:"chip-amb-botó-de-tancar",children:"Chip amb botó de tancar"}),`
+`,e.jsx(i,{of:p}),`
+`,e.jsx(s.h3,{id:"chip-amb-icona-i-botó-de-tancar",children:"Chip amb icona i botó de tancar"}),`
+`,e.jsx(i,{of:m}),`
+`,e.jsx(s.h3,{id:"chip-només-icona",children:"Chip només icona"}),`
+`,e.jsx(i,{of:b}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h4,{id:"changelog",children:"Changelog"}),`
+`,e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Versió"}),e.jsx(s.th,{children:"Canvis"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"2.5.1"})}),e.jsx(s.td,{children:"S'ha corregit un error a l'espaiat de la variant només icona."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"2.5.0"})}),e.jsx(s.td,{children:"S'ha corregit un error en els paddings del component quan s'activa la icona o el botó de tancar."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"2.2.1"})}),e.jsx(s.td,{children:"S'han afegit la mida xs."})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.strong,{children:"2.0.0"})}),e.jsx(s.td,{children:"Refactor v2."})]})]})]})]})}function N(n={}){const{wrapper:s}={...t(),...n.components};return s?e.jsx(s,{...n,children:e.jsx(r,{...n})}):r(n)}export{N as default};
