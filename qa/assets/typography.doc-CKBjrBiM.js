173be0457dab47be75be5b094918f595815b803d
ruben.gomez <ruben.gomez@accenture.com>
2025-07-04 09:47:50 +0200

    Update rc builds

---
+++ qa/assets/typography.doc-CKBjrBiM.js
@@ -0,0 +1,36 @@
+import{j as e,M as d,T as l,C as s,a}from"./index-CwUnIK6l.js";import{useMDXComponents as r}from"./index--VIXWnPk.js";import{S as o,P as c,H as h,a as x,B as j}from"./typography.stories-CTjzPAk8.js";import"./iframe-CdaQSJPn.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-C_pJYAGD.js";import"./index-DxHf6ONh.js";import"./index-DrFu-skq.js";import"./lit-element-Dloa6E9d.js";import"./lit-html-D6a8R3xZ.js";import"./typography-DmufV_rr.js";import"./property-BpSxuat7.js";import"./reset.style-B0CQ8619.js";import"./property-types-CkH-zh1n.js";import"./class-map-t53NE6z1.js";import"./static-BTPASdtP.js";function i(n){const t={a:"a",blockquote:"blockquote",code:"code",h3:"h3",h4:"h4",li:"li",p:"p",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...r(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx("style",{children:`
+    .innerZoomElementWrapper #root-inner {
+      display: grid;
+      gap: 16px;
+    }
+  `}),`
+`,e.jsx(d,{of:o}),`
+`,e.jsxs("div",{className:"dss-sb-title",children:[e.jsx(l,{}),e.jsx("dss-badge",{text:"dss-typography",size:"md",state:"info",outlined:!0,hideIcon:!0})]}),`
+`,e.jsx(t.p,{children:`La tipografia és una manera d'utilitzar el text com a visual per transmetre un   missatge de marca.
+En aquest sistema, aquest element de disseny és important per construir personalitat, transmetre un missatge i també per captar
+l'atenció de l'espectador, construir una jerarquia, el reconeixement de la marca, l'harmonia i establir el valor i el to d'una marca.`}),`
+`,e.jsx(t.p,{children:"Mitjançant aquest component podrem definir tipografies de tipus: títol (headline), subtítol (subtitle) i cos (body)."}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsx(t.p,{children:"Aquest és el exemple que es troba al playground. Tota modificació es veurà reflectida al següent bloc."}),`
+`]}),`
+`,e.jsx(s,{of:c}),`
+`,e.jsx(a,{}),`
+`,e.jsx(t.h3,{id:"propietats",children:"Propietats"}),`
+`,e.jsx("div",{className:"dss-sb-properties",children:e.jsxs(t.table,{children:[e.jsx(t.thead,{children:e.jsxs(t.tr,{children:[e.jsx(t.th,{children:"Name"}),e.jsx(t.th,{children:"Type"}),e.jsx(t.th,{children:"Default"}),e.jsx(t.th,{children:"Description"})]})}),e.jsxs(t.tbody,{children:[e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:"tag"})}),e.jsx(t.td,{children:e.jsx(t.code,{children:"string"})}),e.jsx(t.td,{children:"'div'"}),e.jsx(t.td,{children:"Etiqueta HTML en què s'inclourà el text. Per exemple: 'h1', 'h2', 'h3', 'h4', 'div', 'p', 'span'"})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:"variant"})}),e.jsx(t.td,{children:e.jsx(t.code,{children:"string"})}),e.jsx(t.td,{children:"'body-2'"}),e.jsx(t.td,{children:"Variant de text a triar. Pot ser: 'headline-1', 'headline-2', 'headline-3', 'headline-4', 'subtitle-1', 'subtitle-2', 'subtitle-3', 'subtitle-4', 'body-1', 'body-2', 'body-3'"})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:"fontWeight"})}),e.jsx(t.td,{children:e.jsx(t.code,{children:"string"})}),e.jsx(t.td,{children:"'inherit'"}),e.jsx(t.td,{children:"Estil font-weight que s'assignarà al text, només per a les variants de tipus body. Pot ser: 'ihnerit', 'regular', 'semibold', 'bold'"})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:"fontColor"})}),e.jsx(t.td,{children:e.jsx(t.code,{children:"string"})}),e.jsx(t.td,{children:"-"}),e.jsx(t.td,{children:"Sobreescriu el color neutre per defecte de la variant seleccionada. Pot ser: '500', '700', '900'"})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:"underline"})}),e.jsx(t.td,{children:e.jsx(t.code,{children:"boolean"})}),e.jsx(t.td,{children:"false"}),e.jsx(t.td,{children:"Assigna al text l'estil text-decoration: underline, només per a les variants de tipus body."})]})]})]})}),`
+`,e.jsx(t.p,{children:"(*) El text a mostrar es defineix dins del component com a slot."}),`
+`,e.jsx("br",{}),`
+`,e.jsx("h3",{children:"Variants"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"#headline",children:"Headline"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"#subtitle",children:"Subtitle"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"#body",children:"Body"})}),`
+`]}),`
+`,e.jsx(t.h3,{id:"headline",children:"Headline"}),`
+`,e.jsx(s,{of:h}),`
+`,e.jsx(t.h3,{id:"subtitle",children:"Subtitle"}),`
+`,e.jsx(s,{of:x}),`
+`,e.jsx(t.h3,{id:"body",children:"Body"}),`
+`,e.jsx(s,{of:j}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t.h4,{id:"changelog",children:"Changelog"}),`
+`,e.jsxs(t.table,{children:[e.jsx(t.thead,{children:e.jsxs(t.tr,{children:[e.jsx(t.th,{children:"Versió"}),e.jsx(t.th,{children:"Canvis"})]})}),e.jsxs(t.tbody,{children:[e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:"2.5.1"})}),e.jsx(t.td,{children:"S'han reanomenat la font-family i els fitxers que defineixen la tipografia Open Sans per evitar el conflicte amb el bloqueig de peticions a Google API."})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:"2.4.0"})}),e.jsx(t.td,{children:"S'afegeix la propietat fontColor."})]}),e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:"2.2.0"})}),e.jsx(t.td,{children:"Nou web component."})]})]})]})]})}function T(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i(n)}export{T as default};
