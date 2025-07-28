21011503d09cf7b00d7a7a65f9b6ba8ae88f4c57
ruben.gomez <ruben.gomez@accenture.com>
2025-07-28 13:16:17 +0200

    Update QA to v2.7.2

---
+++ qa/assets/changelog-17X.doc-DE7-FjHK.js
@@ -0,0 +1,23 @@
+import{j as e,M as a}from"./index-B03RRnwf.js";import{useMDXComponents as l}from"./index-CvryOKBv.js";import"./DocsRenderer-CFRXHY34-BkWpt6w1.js";import"./iframe-LtfO51Ti.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-C_pJYAGD.js";import"./index-DxHf6ONh.js";import"./index-DrFu-skq.js";function i(s){const n={h2:"h2",h3:"h3",h4:"h4",hr:"hr",li:"li",ul:"ul",...l(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"Documentació/Changelog v1/1.7.X"}),`
+`,e.jsx(n.h2,{id:"changelog",children:"Changelog"}),`
+`,e.jsx("br",{}),`
+`,e.jsx("div",{class:"dss-grid",children:e.jsx("div",{class:"dss-col--6 dss-col--sm-4 sb-changelog-col",children:e.jsx("dss-badge",{size:"lg",state:"ideal",text:"Versió: 1.7.X",hideIcon:!0})})}),`
+`,e.jsx("br",{}),`
+`,e.jsx("div",{class:"dss-grid",children:e.jsx("div",{class:"dss-col--8 dss-col--sm-5",children:e.jsx(n.h3,{id:"informe-de-registre-de-canvis",children:"Informe de registre de canvis"})})}),`
+`,e.jsx("div",{class:"dss-grid",children:e.jsxs("div",{class:"dss-col--8 dss-col--sm-12",children:[e.jsx(n.h3,{id:"171",children:"1.7.1"}),e.jsx(n.h4,{id:"custom-table",children:"Custom table"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Se ha corregido un error en el caso de uso donde los datos se paginan/segmentan desde el propio backend, que estaba produciendo un bucle infinito al modificar los datos cuando se hacía click en el paginador."}),`
+`,e.jsx(n.li,{children:"Se ha corregido un error por el que el orden de las columnas de tipo número no se mantenía al cambiar el estado del paginador."}),`
+`]}),e.jsx(n.hr,{}),e.jsx(n.h3,{id:"170",children:"1.7.0"}),e.jsx(n.h4,{id:"button",children:"Button"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"S'ha afegit el disseny per als botons 'alternative-light' i 'alternative-dark'."}),`
+`]}),e.jsx(n.h4,{id:"custom-table-1",children:"Custom table"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"S'ha creat una nova versió del component per a les taules que conté tota la funcionalitat del component anterior però permetent una major flexibilitat a l'hora de maquetar l'interior de la taula i un control total sobre la interacció amb els elements de cada fila."}),`
+`]}),e.jsx(n.h4,{id:"list-menu",children:"List menu"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"S'ha corregit l'espaiat entre els elements títol, descripció i llista."}),`
+`]}),e.jsx(n.h4,{id:"modal",children:"Modal"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"S'ha afegit un slot a la capçalera del modal que permet afegir components interactius com el Content Switcher."}),`
+`,e.jsx(n.li,{children:"S'ha afegit una opció per ocultar la icona de tancar l'espai per a aquells casos en què ja hi hagi un botó tancar al footer."}),`
+`]}),e.jsx(n.h4,{id:"notification-badge",children:"Notification badge"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Nou component per mostrar notificacions numèriques amb els seus estats corresponents."}),`
+`]}),e.jsx(n.h4,{id:"button-link-discontinued",children:"Button link (Discontinued)"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Nou component simple per donar un estil específic als enllaços."}),`
+`]})]})})]})}function x(s={}){const{wrapper:n}={...l(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(i,{...s})}):i(s)}export{x as default};
