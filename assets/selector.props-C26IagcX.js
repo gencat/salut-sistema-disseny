8f0016935976ada9775cc609dadf7083017b2333
ruben.gomez <ruben.gomez@accenture.com>
2026-07-28 12:06:37 +0200

    Update native vs custom event type

---
+++ assets/selector.props-C26IagcX.js
@@ -0,0 +1,3 @@
+import{j as t,M as r}from"./blocks-DGLXn5H1.js";import{useMDXComponents as o}from"./index-Dog3P8rX.js";import{S as i}from"./selector.stories-t2xMslWq.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-COSZQ9iZ.js";function s(e){const n={h3:"h3",...o(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:i,name:"Props"}),`
+`,t.jsxs("div",{className:"dss-sb-title",children:[t.jsx(n.h3,{id:"propietats",children:"Propietats"}),t.jsx("component-title",{children:"dss-selector"})]}),`
+`,t.jsx("br",{})]})}function j(e={}){const{wrapper:n}={...o(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(s,{...e})}):s(e)}export{j as default};
---
+++ assets/skeleton.log-DGE9gMNy.js
@@ -0,0 +1,4 @@
+import{j as s,M as i}from"./blocks-DGLXn5H1.js";import{useMDXComponents as o}from"./index-Dog3P8rX.js";import{S as r}from"./skeleton.stories-DCO2Dfdi.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-COSZQ9iZ.js";function e(n){const t={h3:"h3",...o(),...n.components};return s.jsxs(s.Fragment,{children:[s.jsx(i,{of:r,name:"Changelog"}),`
+`,s.jsxs("div",{className:"dss-sb-title",children:[s.jsx(t.h3,{id:"historial-de-canvis",children:"Historial de canvis"}),s.jsx("component-title",{children:"dss-skeleton"})]}),`
+`,s.jsx("br",{}),`
+`,s.jsx("div",{className:"dss-sb-changelog"})]})}function h(n={}){const{wrapper:t}={...o(),...n.components};return t?s.jsx(t,{...n,children:s.jsx(e,{...n})}):e(n)}export{h as default};
