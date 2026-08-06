75f313e3597a369d1fc4b5d8108b82a9dd40ea9e
ruben.gomez <ruben.gomez@accenture.com>
2026-08-06 10:02:16 +0200

    Update duplicate prop in header-menu-professional

---
+++ assets/formatter-QJ4M4OGQ-ZRrMyhsr.js
@@ -0,0 +1,1 @@
+import{e as i,f as m}from"./blocks-Dagl8dgy.js";import{b as o}from"./iframe-BgTKh_jv.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(m(),1),_=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{_ as formatter};
